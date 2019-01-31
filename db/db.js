3ae931b35b726f6bec5626cb436b623970433666
jay-stpl <jay.singh@stplinc.com>
2019-01-31 16:49:20 +0530

    Node server is uploaded to gitHub

---
+++ db/db.js
@@ -0,0 +1,29 @@
+const todos =  [
+    {
+        id: 1,
+        title: "lunch",
+        description: "Go for lunc by 2pm"
+    },
+    {
+        id: 2,
+        title: "lunch 2",
+        description: "Go for lunc by 2pm"
+    },
+    {
+        id: 3,
+        title: "lunch 3",
+        description: "Go for lunc by 2pm"
+    },
+    {
+        id: 4,
+        title: "lunch 4",
+        description: "Go for lunc by 2pm"
+    },
+    {
+        id: 5,
+        title: "lunch 5",
+        description: "Go for lunc by 2pm"
+    },
+];
+
+export default todos;
---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by jay on 29/1/19.
+ */
+require('babel-register')({
+    presets: [ 'es2015' ]
+});
+
+require('./server');
